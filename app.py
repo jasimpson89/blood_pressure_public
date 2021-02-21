2e506261f3632eb64a7e74d11f6f7395c6e787da
James Simpson <jasimpson89@gmail.com>
2021-02-21 23:54:26 +0000

    testing the plotly example

--- app.py
+++ app.py
@@ -1,44 +1,29 @@
+import os
+
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
-import plotly.graph_objs as go
-import pandas as pd
-from dash.dependencies import Input, Output
-import urllib.request
-#initiating the app
-app = dash.Dash()
+
+external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
+
+app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
+
 server = app.server
 
-url_onedrive = "https://onedrive.live.com/download.aspx?resid=76BCA2A8519465B9!64241&ithint=file%2cxlsx&authkey=!AL2olPlMJ4XfQqQ"
-
-# urllib.request.urlretrieve(url_onedrive, "test.xlsx")
-
-# xlsx_file = "/Users/jsimpson/OneDrive/blood_pressure/bp_v_weight_v2.xlsx"
-
-
-df = pd.read_excel( "./bp_v_weight_v2.xlsx", sheet_name="new data")
-
-
 app.layout = html.Div([
-                html.Div([
-                    dcc.Graph(id='mpg-scatter',
-                              figure={
-                                  'data':[go.Scatter(
-                                      x=df['date'],
-                                      y=df['bp1'],
-
-                                      mode='markers'
-                                  )],
-                                  'layout':go.Layout(
-                                      title='Bp1 over time',
-                                      xaxis={'title':'Date'},
-                                      yaxis={'title':'Blood pressure (mmHg)'},
-                                      hovermode='closest'
-                                   )}
-                    )
-                ],style={'width':'50%','display':'inline-block'}),
+    html.H2('Hello World'),
+    dcc.Dropdown(
+        id='dropdown',
+        options=[{'label': i, 'value': i} for i in ['LA', 'NYC', 'MTL']],
+        value='LA'
+    ),
+    html.Div(id='display-value')
 ])
+
+@app.callback(dash.dependencies.Output('display-value', 'children'),
+              [dash.dependencies.Input('dropdown', 'value')])
+def display_value(value):
+    return 'You have selected "{}"'.format(value)
+
 if __name__ == '__main__':
-
-
-    app.run_server()
+    app.run_server(debug=True)
