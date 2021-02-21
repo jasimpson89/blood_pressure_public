711962b9362def9f540f645200559404dc500e25
James Simpson <jasimpson89@gmail.com>
2021-02-21 23:58:27 +0000

    further testing

--- app.py
+++ app.py
@@ -12,18 +12,9 @@ server = app.server
 
 app.layout = html.Div([
     html.H2('Hello World'),
-    dcc.Dropdown(
-        id='dropdown',
-        options=[{'label': i, 'value': i} for i in ['LA', 'NYC', 'MTL']],
-        value='LA'
-    ),
-    html.Div(id='display-value')
+
 ])
 
-@app.callback(dash.dependencies.Output('display-value', 'children'),
-              [dash.dependencies.Input('dropdown', 'value')])
-def display_value(value):
-    return 'You have selected "{}"'.format(value)
 
 if __name__ == '__main__':
     app.run_server(debug=True)
