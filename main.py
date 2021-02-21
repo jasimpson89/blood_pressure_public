e4f39a7d340d7e5ebd8975d8f8e3ff4b9ef1ff8e
James Simpson <jasimpson89@gmail.com>
2021-02-21 22:27:45 +0000

    Updated smaller requirements
    fixed the plotting it was broken

--- main.py
+++ main.py
@@ -1,41 +1,35 @@
-import plotly.graph_objects as go
-from plotly.subplots import make_subplots
-import pandas as pd
-import requests
-from datetime import datetime
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
+import plotly.graph_objs as go
+import pandas as pd
 from dash.dependencies import Input, Output
-import plotly.express as px
-
 
 #initiating the app
 app = dash.Dash()
+
 xlsx_file = "/Users/jsimpson/OneDrive/blood_pressure/bp_v_weight_v2.xlsx"
 df = pd.read_excel(xlsx_file, sheet_name="new data")
-#defining the layout
+
+
 app.layout = html.Div([
                 html.Div([
                     dcc.Graph(id='mpg-scatter',
                               figure={
                                   'data':[go.Scatter(
                                       x=df['date'],
-                                      y=[df['bp1']],
-                                      # text=df['name'],
+                                      y=df['bp1'],
+
                                       mode='markers'
                                   )],
                                   'layout':go.Layout(
-                                      title='MPG vs Model Year',
-                                      xaxis={'title':'Model Year'},
-                                      yaxis={'title':'MPG'},
+                                      title='Bp1 over time',
+                                      xaxis={'title':'Date'},
+                                      yaxis={'title':'Blood pressure (mmHg)'},
                                       hovermode='closest'
                                    )}
                     )
                 ],style={'width':'50%','display':'inline-block'}),
-
-
 ])
-#running the app
 if __name__ == '__main__':
     app.run_server()
