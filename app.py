92a2fe7d4976bbfadecefca3d6a7d40c995632dc
James Simpson <jasimpson89@gmail.com>
2021-02-22 00:18:33 +0000

    further testing

--- app.py
+++ app.py
@@ -4,13 +4,13 @@ import dash
 import dash_core_components as dcc
 import dash_html_components as html
 import plotly.graph_objs as go
-
+import pandas as pd
 
 
 app = dash.Dash()
 
 server = app.server
-
+df = pd.read_excel( "./bp_v_weight_v2.xlsx", sheet_name="new data")
 app.layout = html.Div([
     html.Div([
         dcc.Graph(id='mpg-scatter',
