bb3de8e0ba03f76b8291933696d07861700c89d7
James Simpson <jasimpson89@gmail.com>
2021-02-22 00:55:31 +0000

    further testing

--- app.py
+++ app.py
@@ -10,15 +10,14 @@ import pandas as pd
 app = dash.Dash()
 
 server = app.server
-url="https://raw.githubusercontent.com/jasimpson89/blood_pressure/master/bp_v_weight_v2.csv?token=AO6O4MQ57R3ZZQN764HIFPTAGL7LI"
 df = pd.read_csv("./bp_v_weight_v2.csv",sep=",")
 app.layout = html.Div([
     html.Div([
         dcc.Graph(id='mpg-scatter',
                   figure={
                       'data': [go.Scatter(
-                          x=[1,2,3],
-                          y=[10,20,30],
+                          x=df["date"],
+                          y=df["bp1"],
 
                           mode='markers'
                       )],
