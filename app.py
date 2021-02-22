ded1c8cd7e09d5a51d0598b279db85fc42d1bac1
James Simpson <jasimpson89@gmail.com>
2021-02-22 00:52:17 +0000

    further testing

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ app = dash.Dash()
 
 server = app.server
 url="https://raw.githubusercontent.com/jasimpson89/blood_pressure/master/bp_v_weight_v2.csv?token=AO6O4MQ57R3ZZQN764HIFPTAGL7LI"
-df = df = pd.read_csv(url,sep=",")
+df = pd.read_csv("./bp_v_weight_v2.csv",sep=",")
 app.layout = html.Div([
     html.Div([
         dcc.Graph(id='mpg-scatter',
