eba3f5eaa3784f63be5b53f940d14aa3400318a1
James Simpson <jasimpson89@gmail.com>
2021-02-22 01:18:54 +0000

    trying plotly express

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ import dash_core_components as dcc
 import dash_html_components as html
 import plotly.graph_objs as go
 import pandas as pd
+import plotly.express as px
 
 
 app = dash.Dash()
@@ -12,24 +13,13 @@ app = dash.Dash()
 server = app.server
 df = pd.read_csv("./bp_v_weight_v2.csv",sep=",")
 
+fig = px.scatter(df, x="date", y=["bp1","bp2"])
 
 
 app.layout = html.Div([
     html.Div([
         dcc.Graph(id='bp1-scatter',
-                  figure={
-                      'data': [go.Scatter(
-                          x=df["date"],
-                          y=df["bp1"],
-
-                          mode='markers'
-                      )],
-                      'layout': go.Layout(
-                          title='Bp1 over time',
-                          xaxis={'title': 'Date'},
-                          yaxis={'title': 'Blood pressure (mmHg)'},
-                          hovermode='closest'
-                      )}
+                  figure=fig
                   )
     ], style={'width': '50%', 'display': 'inline-block'}),
     html.Div([
