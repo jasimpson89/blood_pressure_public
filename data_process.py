01c855c39ed129edcebb8e43a2cc8bb82b251221
James Simpson <jasimpson89@gmail.com>
2021-03-07 21:07:24 +0000

    Fixed a bug introduced in merging the monthly averaged dataframe.
    It happened when lengths of the dataframes containing the blood pressure (which is averaged on a monthly basis) and the exercise dataframe which is only given on a monthly time basis.
    
    Solved by just recording the first and last matched indices between to the dataframes

--- data_process.py
+++ data_process.py
@@ -1,6 +1,6 @@
 import pandas as pd
 import numpy as np
-
+import math
 def monthly_avg(df):
     # this generates a group object, this groups byn month/year (i.e. both) if you wanted to group on months across many
     # years you would need something else
@@ -20,21 +20,39 @@ def match_df_month_avg(df_monthly_avg,df):
     systolic = []
     diastolic = []
     weight = []
+    # flag used for detecting the first matching index
+    first_flag = True
+
+
     for month_s,year_s,bp1,bp2,weight_tot in zip(df_monthly_avg.index.month,df_monthly_avg.index.year,df_monthly_avg["systolic"],df_monthly_avg["diastolic"],df_monthly_avg["weight (kg)"]):
-        for month_m,year_m in zip(df.index.month,df.index.year):
-            if month_s == month_m and year_s == year_m:
-                systolic.append(bp1)
-                diastolic.append(bp2)
-                weight.append(weight_tot)
-                continue
+        # nan_flag = True
+        for date_m, month_m,year_m in zip(df.index, df.index.month,df.index.year):
+            if date_m >= df_monthly_avg.index[0] and date_m <= df_monthly_avg.index[-1]:
+                if month_s == month_m and year_s == year_m:
+                    systolic.append(bp1)
+                    diastolic.append(bp2)
+                    weight.append(weight_tot)
+                    # nan_flag = False
+                if first_flag == True:
+                    # record this to slice the array with
+                    begin_idx = date_m
+                    first_flag = False
+                else:
+                    end_idx = date_m
 
+    # Account for the fact len(df_monthly_avg) could be a different to len(df)
+    # The date ranges which 'df_monthly_avg' and 'df' cover maybe different. We only care about months we have BP data for
+    df_new = (df[begin_idx:end_idx]).copy()
 
-    df['avg monthly systolic'] = systolic
-    df['avg monthly diastolic'] = diastolic
-    df['avg monthly weight (kg)'] = weight
+    """
+    ITS STILL FAILING HERE THE LENGTHS OF TH ARARY ARE WRONG 
+    """
+    df_new['avg monthly systolic'] = systolic
+    df_new['avg monthly diastolic'] = diastolic
+    df_new['avg monthly weight (kg)'] = weight
 
     # anything which has NaN can't really be used
     # if any row has a NaN on it get rid of it
-    df.dropna(axis=0,how='any')
+    df_new.dropna(axis=0,how='any')
 
-    return df
+    return df_new
